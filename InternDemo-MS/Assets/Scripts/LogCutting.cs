44864bf1e2f3a0c4ffc8e3514f73ca56d063b192
msahinn21 <46489634+msahinn21@users.noreply.github.com>
2020-09-25 17:41:33 +0300

    3

---
+++ InternDemo-MS/Assets/Scripts/LogCutting.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogCutting : MonoBehaviour
+{
+    public GameObject CutVersion;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Instantiate(CutVersion, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ InternDemo-MS/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+	public float moveSpeed = 300;
+	public GameObject character;
+
+	private Rigidbody characterBody;
+	private float ScreenWidth;
+
+
+	void Start()
+	{
+		ScreenWidth = Screen.width;
+		characterBody = character.GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+		int i = 0;
+		while (i < Input.touchCount)
+		{
+			if (Input.GetTouch(i).position.x > ScreenWidth / 2)
+			{
+				RunCharacter(1.0f);
+			}
+			if (Input.GetTouch(i).position.x < ScreenWidth / 2)
+			{
+				RunCharacter(-1.0f);
+			}
+			++i;
+		}
+	}
+	void FixedUpdate()
+	{
+#if UNITY_EDITOR
+		RunCharacter(Input.GetAxis("Horizontal"));
+#endif
+	}
+
+	private void RunCharacter(float horizontalInput)
+	{
+		characterBody.AddForce(new Vector2(horizontalInput * moveSpeed * Time.deltaTime, 0));
+
+	}
+}
